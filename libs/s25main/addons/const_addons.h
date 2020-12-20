JonathanSteinbuch/s25client

Many small code improvements, correct remaining time calculation and display, different approach towards making whole map visible, renamed game length addon. Thanks for the extensive review, @Flamefire.

libs/s25main/addons/const_addons.h
@@ -81,7 +81,7 @@ ENUM_WITH_STRING(AddonId, LIMIT_CATAPULTS = 0x00000000, INEXHAUSTIBLE_MINES = 0x
                  FRONTIER_DISTANCE_REACHABLE = 0x00D0000, COINS_CAPTURED_BLD = 0x00D0001,
                  DEMOLISH_BLD_WO_RES = 0x00D0002,
 
-                 PEACEFUL = 0x00E0000, DURABLE_GEOLOGIST_SIGNS = 0x00E0001, GAME_LENGTH = 0x00E0002)
+                 PEACEFUL = 0x00E0000, DURABLE_GEOLOGIST_SIGNS = 0x00E0001, ECONOMY_MODE_GAME_LENGTH = 0x00E0002)
 //-V:AddonId:801
 
 enum class AddonGroup : unsigned

Farmer-Markus/s25client-android

libs/s25main/addons/const_addons.h
@@ -81,7 +81,7 @@ ENUM_WITH_STRING(AddonId, LIMIT_CATAPULTS = 0x00000000, INEXHAUSTIBLE_MINES = 0x
                  FRONTIER_DISTANCE_REACHABLE = 0x00D0000, COINS_CAPTURED_BLD = 0x00D0001,
                  DEMOLISH_BLD_WO_RES = 0x00D0002,
 
-                 PEACEFULMODE = 0x00E0000, DURABLE_GEOLOGIST_SIGNS = 0x00E0001)
+                 PEACEFULMODE = 0x00E0000, DURABLE_GEOLOGIST_SIGNS = 0x00E0001, ECONOMY_MODE_GAME_LENGTH = 0x00E0002)
 //-V:AddonId:801
 
 enum class AddonGroup : unsigned