Return-To-The-Roots/s25client

commit 7140756b88567f6bb0ab406274818135affa169a

Increase test coverage of changes
Excludes windows and desktops as those are to hard to test

libs/s25main/addons/AddonEconomyModeGameLength.h
@@ -18,30 +18,36 @@
 #pragma once
 
 #include "AddonList.h"
+#include "RTTR_Assert.h"
+#include "helpers/containerUtils.h"
+#include "helpers/format.hpp"
+#include "helpers/make_array.h"
 #include "mygettext/mygettext.h"
-#include <boost/format.hpp>
 
 /**
  *  Addon allows users to adjust the game length (for Economy Mode)
  */
-const unsigned int AddonEconomyModeGameLengthList[] = {0, 15, 30, 60, 90, 120, 150, 180, 240, 480}; // length in minutes
+constexpr auto AddonEconomyModeGameLengthList =
+  helpers::make_array(0, 15, 30, 60, 90, 120, 150, 180, 240, 480); // length in minutes
 
 class AddonEconomyModeGameLength : public AddonList
 {
+    static std::vector<std::string> makeOptions()
+    {
+        std::vector<std::string> result = {_("unlimited")};
+        for(const int duration : AddonEconomyModeGameLengthList)
+            result.push_back(helpers::format(_("%1%min"), duration));
+        return result;
+    }
+
 public:
     AddonEconomyModeGameLength()
         : AddonList(AddonId::ECONOMY_MODE_GAME_LENGTH, AddonGroup::Economy | AddonGroup::GamePlay,
                     _("Economy Mode: Game Length"),
-                    _("Adjust the time after which the economy mode victory condition is checked."),
-                    {_("unlimited"), (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[1]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[2]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[3]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[4]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[5]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[6]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[7]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[8]).str(),
-                     (boost::format(_("%1%min")) % AddonEconomyModeGameLengthList[9]).str()},
-                    5)
-    {}
+                    _("Adjust the time after which the economy mode victory condition is checked."), makeOptions(),
+                    helpers::indexOf(AddonEconomyModeGameLengthList, 120))
+
+    {
+        RTTR_Assert(getDefaultStatus() != unsigned(-1));
+    }
 };

libs/s25main/GamePlayer.cpp
@@ -519,7 +519,7 @@ void GamePlayer::RoadDestroyed()
             noRoadNode& wareLocation = *ware->GetLocation();
             noBaseBuilding* wareGoal = ware->GetGoal();
             if(wareGoal && ware->GetNextDir() == RoadPathDirection::NorthWest
-               && wareLocation.GetPos() == wareGoal->GetFlag()->GetPos()
+               && wareLocation.GetPos() == wareGoal->GetFlagPos()
                && ((wareGoal->GetBuildingType() != BuildingType::Storehouse
                     && wareGoal->GetBuildingType() != BuildingType::Headquarters
                     && wareGoal->GetBuildingType() != BuildingType::HarborBuilding)
@@ -2151,13 +2151,13 @@ std::vector<nobBaseWarehouse*> GamePlayer::GetWarehousesForTrading(const nobBase
     if(goalWh.GetPlayer() == GetPlayerId())
         return result;
 
-    const MapPoint goalFlagPos = goalWh.GetFlag()->GetPos();
+    const MapPoint goalFlagPos = goalWh.GetFlagPos();
 
     TradePathCache& tradePathCache = gwg.GetTradePathCache();
     for(nobBaseWarehouse* wh : buildings.GetStorehouses())
     {
         // Is there a trade path from this warehouse to wh? (flag to flag)
-        if(tradePathCache.PathExists(wh->GetFlag()->GetPos(), goalFlagPos, GetPlayerId()))
+        if(tradePathCache.pathExists(wh->GetFlagPos(), goalFlagPos, GetPlayerId()))
             result.push_back(wh);
     }
 
@@ -2200,7 +2200,7 @@ void GamePlayer::Trade(nobBaseWarehouse* goalWh, const boost::variant<GoodType, 
     if(!IsAlly(goalWh->GetPlayer()))
         return;
 
-    const MapPoint goalFlagPos = goalWh->GetFlag()->GetPos();
+    const MapPoint goalFlagPos = goalWh->GetFlagPos();
 
     std::vector<nobBaseWarehouse*> whs(buildings.GetStorehouses().begin(), buildings.GetStorehouses().end());
     std::sort(whs.begin(), whs.end(), WarehouseDistanceComparator(*goalWh, gwg));
@@ -2218,13 +2218,13 @@ void GamePlayer::Trade(nobBaseWarehouse* goalWh, const boost::variant<GoodType, 
         const unsigned actualCount = std::min(available, count);
 
         // Find a trade path from flag to flag
-        TradeRoute tr(gwg, GetPlayerId(), wh->GetFlag()->GetPos(), goalFlagPos);
+        TradeRoute tr(gwg, GetPlayerId(), wh->GetFlagPos(), goalFlagPos);
 
         // Found a path?
         if(tr.IsValid())
         {
             // Add to cache for future searches
-            tradePathCache.AddEntry(tr.GetTradePath(), GetPlayerId());
+            tradePathCache.addEntry(tr.GetTradePath(), GetPlayerId());
 
             wh->StartTradeCaravane(what, actualCount, tr, goalWh);
             count -= available;

libs/s25main/TradePathCache.cpp
@@ -21,11 +21,11 @@
 #include "world/GameWorldGame.h"
 #include "gameData/GameConsts.h"
 
-bool TradePathCache::PathExists(const MapPoint start, const MapPoint goal, const unsigned char player)
+bool TradePathCache::pathExists(const MapPoint start, const MapPoint goal, const unsigned char player)
 {
     RTTR_Assert(start != goal);
 
-    int entryIdx = FindEntry(start, goal, player);
+    int entryIdx = findEntry(start, goal, player);
     if(entryIdx >= 0)
     {
         // Found an entry --> Check if the route is still valid
@@ -48,11 +48,11 @@ bool TradePathCache::PathExists(const MapPoint start, const MapPoint goal, const
     if(!gwg.FindTradePath(start, goal, player, std::numeric_limits<unsigned>::max(), false, &route))
         return false;
 
-    AddEntry(TradePath(start, goal, std::move(route)), player);
+    addEntry(TradePath(start, goal, std::move(route)), player);
     return true;
 }
 
-int TradePathCache::FindEntry(const MapPoint start, const MapPoint goal, const PlayerIdx player) const
+int TradePathCache::findEntry(const MapPoint start, const MapPoint goal, const PlayerIdx player) const
 {
     const GamePlayer& thisPlayer = gwg.GetPlayer(player);
 
@@ -70,11 +70,11 @@ int TradePathCache::FindEntry(const MapPoint start, const MapPoint goal, const P
     return -1;
 }
 
-void TradePathCache::AddEntry(TradePath path, const unsigned char player)
+void TradePathCache::addEntry(TradePath path, const unsigned char player)
 {
     Entry entry{player, gwg.GetEvMgr().GetCurrentGF(), std::move(path)};
 
-    int idx = FindEntry(entry.path.start, entry.path.goal, player);
+    int idx = findEntry(entry.path.start, entry.path.goal, player);
     if(idx < 0)
     {
         // None found --> Find a new spot

libs/s25main/TradePathCache.h
@@ -35,12 +35,13 @@ class TradePathCache
 
     const GameWorldGame& gwg;
     boost::container::static_vector<Entry, 10> paths;
-    int FindEntry(MapPoint start, MapPoint goal, PlayerIdx player) const;
+    int findEntry(MapPoint start, MapPoint goal, PlayerIdx player) const;
 
 public:
     TradePathCache(const GameWorldGame& gwg) : gwg(gwg) {}
 
-    void Clear() { paths.clear(); }
-    bool PathExists(MapPoint start, MapPoint goal, PlayerIdx player);
-    void AddEntry(TradePath path, PlayerIdx player);
+    void clear() { paths.clear(); }
+    unsigned size() const { return paths.size(); }
+    bool pathExists(MapPoint start, MapPoint goal, PlayerIdx player);
+    void addEntry(TradePath path, PlayerIdx player);
 };

libs/s25main/Ware.cpp
@@ -357,7 +357,7 @@ Ware::RouteParams Ware::CalcPathToGoal(const noBaseBuilding& newgoal) const
     {
         // in case the ware is right in front of the goal building the ware has to be moved away 1 flag and then back
         // because non-warehouses cannot just carry in new wares they need a helper to do this
-        if(possibledir == RoadPathDirection::NorthWest && newgoal.GetFlag()->GetPos() == location->GetPos())
+        if(possibledir == RoadPathDirection::NorthWest && newgoal.GetFlagPos() == location->GetPos())
         {
             for(const auto dir : helpers::EnumRange<Direction>{})
             {

libs/s25main/buildings/noBaseBuilding.h
@@ -68,7 +68,7 @@ public:
 
     /// Ermittelt die Flagge, die vor dem Gebäude steht
     noFlag* GetFlag() const;
-    /// Same as GetFlag()->GetPos()
+    /// Same as GetFlagPos()
     MapPoint GetFlagPos() const;
 
     /// Return the offset of the door, which is also where people disappear into the building, the builder is building

libs/s25main/buildings/nobMilitary.cpp
@@ -1332,7 +1332,7 @@ bool nobMilitary::IsFarAwayCapturer(nofAttacker* attacker)
 
 void nobMilitary::CallNextFarAwayCapturer(nofAttacker* attacker)
 {
-    const MapPoint flagPos = GetFlag()->GetPos();
+    const MapPoint flagPos = GetFlagPos();
     unsigned minLength = std::numeric_limits<unsigned>::max();
     nofAttacker* bestAttacker = nullptr;
     for(auto& far_away_capturer : far_away_capturers)

libs/s25main/figures/nofActiveSoldier.cpp
@@ -103,7 +103,7 @@ void nofActiveSoldier::WalkingHome()
     // Walking home to our military building
 
     // Are we already at the flag?
-    if(GetPos() == building->GetFlag()->GetPos())
+    if(GetPos() == building->GetFlagPos())
     {
         // Enter via the door
         StartWalking(Direction::NorthWest);
@@ -118,7 +118,7 @@ void nofActiveSoldier::WalkingHome()
         gwg->RemoveFigure(pos, this);
         return;
     }
-    const auto dir = gwg->FindHumanPath(pos, building->GetFlag()->GetPos(), 100);
+    const auto dir = gwg->FindHumanPath(pos, building->GetFlagPos(), 100);
     if(dir)
     {
         // Find all sorts of enemies (attackers, aggressive defenders..) nearby

Farmer-Markus/s25client-android

commit 092fba9a8a504eca6a3839a6b806d1d7fd010045

fixed a bug introduced with commit https://github.com/Return-To-The-Roots/s25client/commit/7140756b88567f6bb0ab406274818135affa169a where economy mode game length didn't match the selected length any more

libs/s25main/addons/AddonEconomyModeGameLength.h
@@ -18,14 +18,20 @@
  */
 constexpr auto AddonEconomyModeGameLengthList =
   helpers::make_array<std::chrono::minutes>(0, 15, 30, 60, 90, 120, 150, 180, 240, 480);
+// note that the game length of 0 is used internally for unlimited game length
 
 class AddonEconomyModeGameLength : public AddonList
 {
     static std::vector<std::string> makeOptions()
     {
-        std::vector<std::string> result = {_("unlimited")};
+        std::vector<std::string> result;
         for(const auto duration : AddonEconomyModeGameLengthList)
-            result.push_back(helpers::format("%1%", helpers::withUnit(duration)));
+        {
+            if(duration.count() == 0)
+                result.push_back("unlimited");
+            else
+                result.push_back(helpers::format("%1%", helpers::withUnit(duration)));
+        }
         return result;
     }
 