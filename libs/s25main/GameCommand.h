Return-To-The-Roots/s25client

Add version to game commands
Allow some backwards compatibility especially for the replay such that a
newer version can still read replays from a previous version.
E.g. adding optional addons with additional fields or commands will
still allow old replays to work without issues.

libs/s25main/GameCommand.h
@@ -1,9 +1,10 @@
-// Copyright (C) 2005 - 2021 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (C) 2005 - 2024 Settlers Freaks (sf-team at siedler25.org)
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
+#include "s25util/VersionedSerializer.h"
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 #include <RTTR_Assert.h>
 #include <cstdint>
@@ -13,6 +14,12 @@ class GameWorld;
 class GameCommandFactory;
 
 namespace gc {
+
+struct Deserializer : s25util::VersionedSerializer<Deserializer>
+{
+    using s25util::VersionedSerializer<Deserializer>::VersionedSerializer;
+    static unsigned getCurrentVersion();
+};
 
 class GameCommand;
 // Use this for safely using Pointers to GameCommands
@@ -82,7 +89,7 @@ public:
     }
 
     /// Builds a GameCommand depending on Type
-    static GameCommandPtr Deserialize(Serializer& ser);
+    static GameCommandPtr Deserialize(Deserializer& ser);
 
     /// Serializes this GameCommand
     virtual void Serialize(Serializer& ser) const;

libs/s25main/Replay.cpp
@@ -29,7 +29,7 @@ std::string Replay::GetSignature() const
 ///     - Increase the version in GetVersion
 ///
 /// Changelog:
-/// 1: Unused first CommandType (End) removed
+/// 1: Unused first CommandType (End) removed, GameCommand version added
 static const unsigned currentReplayDataVersion = 1;
 // clang-format on
 
@@ -196,6 +196,10 @@ bool Replay::LoadHeader(const boost::filesystem::path& filepath)
         // TODO(Replay): Move before mapType to have it as early as possible.
         // Previously mapType was an unsigned short, i.e. in little endian the 2nd byte was always unused/zero
         subVersion_ = file_.ReadUnsignedChar();
+        if(subVersion_ >= 1)
+            gcVersion_ = file_.ReadUnsignedInt();
+        else
+            gcVersion_ = 0;
 
         if(mapType_ == MapType::Savegame)
         {
@@ -330,7 +334,7 @@ boost_variant2<Replay::ChatCommand, Replay::GameCommand> Replay::ReadCommand()
     switch(type)
     {
         case CommandType::Chat: return ChatCommand(file_);
-        case CommandType::Game: return GameCommand(file_);
+        case CommandType::Game: return GameCommand(file_, gcVersion_);
         default: throw std::invalid_argument("Invalid command type: " + std::to_string(rttr::enum_cast(type)));
     }
 }
@@ -352,9 +356,9 @@ Replay::ChatCommand::ChatCommand(BinaryFile& file)
       msg(file.ReadLongString())
 {}
 
-Replay::GameCommand::GameCommand(BinaryFile& file)
+Replay::GameCommand::GameCommand(BinaryFile& file, const unsigned version)
 {
-    Serializer ser;
+    gc::Deserializer ser{version};
     ser.ReadFromFile(file);
     player = ser.PopUnsignedChar();
     cmds.Deserialize(ser);

Farmer-Markus/s25client-android

libs/s25main/GameCommand.h
@@ -1,9 +1,10 @@
-// Copyright (C) 2005 - 2021 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (C) 2005 - 2024 Settlers Freaks (sf-team at siedler25.org)
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
+#include "s25util/VersionedDeserializer.h"
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 #include <RTTR_Assert.h>
 #include <cstdint>
@@ -13,6 +14,12 @@ class GameWorld;
 class GameCommandFactory;
 
 namespace gc {
+
+struct Deserializer : s25util::VersionedDeserializer<Deserializer>
+{
+    using s25util::VersionedDeserializer<Deserializer>::VersionedDeserializer;
+    static unsigned getCurrentVersion();
+};
 
 class GameCommand;
 // Use this for safely using Pointers to GameCommands
@@ -82,7 +89,7 @@ public:
     }
 
     /// Builds a GameCommand depending on Type
-    static GameCommandPtr Deserialize(Serializer& ser);
+    static GameCommandPtr Deserialize(Deserializer& ser);
 
     /// Serializes this GameCommand
     virtual void Serialize(Serializer& ser) const;

libs/s25main/Replay.cpp
@@ -29,7 +29,7 @@ std::string Replay::GetSignature() const
 ///     - Increase the version in GetVersion
 ///
 /// Changelog:
-/// 1: Unused first CommandType (End) removed
+/// 1: Unused first CommandType (End) removed, GameCommand version added
 static const uint8_t currentReplayDataVersion = 1;
 // clang-format on
 
@@ -128,6 +128,8 @@ bool Replay::StartRecording(const boost::filesystem::path& filepath, const MapIn
     file_.WriteUnsignedChar(rttr::enum_cast(mapType_));
     // TODO(Replay): Move before mapType
     file_.WriteUnsignedChar(subVersion_ = currentReplayDataVersion);
+    RTTR_Assert(gc::Deserializer::getCurrentVersion() <= std::numeric_limits<decltype(gcVersion_)>::max());
+    file_.WriteUnsignedChar(gcVersion_ = gc::Deserializer::getCurrentVersion());
 
     // For (savegame) format validation
     if(mapType_ == MapType::Savegame)
@@ -192,6 +194,10 @@ bool Replay::LoadHeader(const boost::filesystem::path& filepath)
         // TODO(Replay): Move before mapType to have it as early as possible.
         // Previously mapType was an unsigned short, i.e. in little endian the 2nd byte was always unused/zero
         subVersion_ = file_.ReadUnsignedChar();
+        if(subVersion_ >= 1)
+            gcVersion_ = file_.ReadUnsignedChar();
+        else
+            gcVersion_ = 0;
 
         if(mapType_ == MapType::Savegame)
         {
@@ -326,7 +332,7 @@ boost_variant2<Replay::ChatCommand, Replay::GameCommand> Replay::ReadCommand()
     switch(type)
     {
         case CommandType::Chat: return ChatCommand(file_);
-        case CommandType::Game: return GameCommand(file_);
+        case CommandType::Game: return GameCommand(file_, gcVersion_);
         default: throw std::invalid_argument("Invalid command type: " + std::to_string(rttr::enum_cast(type)));
     }
 }
@@ -348,9 +354,9 @@ Replay::ChatCommand::ChatCommand(BinaryFile& file)
       msg(file.ReadLongString())
 {}
 
-Replay::GameCommand::GameCommand(BinaryFile& file)
+Replay::GameCommand::GameCommand(BinaryFile& file, const unsigned version)
 {
-    Serializer ser;
+    gc::Deserializer ser{version};
     ser.ReadFromFile(file);
     player = ser.PopUnsignedChar();
     cmds.Deserialize(ser);